69e85cc7a36e1a40d9087ea5eb8c2d66afef2b98
gautamkumarvis <gautamvishwakarma349@gmail.com>
2025-08-01 13:39:08 +0530

    soluation4

---
+++ soluation4.java
@@ -0,0 +1,13 @@
+
+public class soluation4 {
+    public static void main(String[] args) {
+        
+    int x = 5, y = 10;
+    if(!(x > 0 && y < 15)) {
+        System.out.println(x + y);
+    }else {
+        System.out.println(x * y);
+    }
+}
+    
+}
