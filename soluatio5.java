7909638f3d0657fb8e764003b28a25d44fdd237b
gautamkumarvis <gautamvishwakarma349@gmail.com>
2025-08-01 14:05:45 +0530

    soluation 7

--- soluatio5.java
+++
@@ -1,5 +0,0 @@
-public class soluatio5 {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ soluation7.java
@@ -0,0 +1,24 @@
+
+
+// sum = 0
+// for i from 1 to 3:
+//     for j from 1 to i:
+//         sum = sum + j
+//     end for
+// end for
+// Print sum
+
+
+public class soluation7{
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= i; j++) {
+                sum = sum + j;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
