39223d05bad3be478d85429042e7d3189a9e4d57
gautamkumarvish <164343017+gautamkumarvish@users.noreply.github.com>
2025-08-01 13:54:52 +0530

    Update soluatio5.java

--- soluatio5.java
+++ soluatio5.java
@@ -1,5 +1,11 @@
-public class soluatio5 {
+public class soluation5 {
     public static void main(String[] args) {
-        
+        int sum = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println(sum);
     }
 }
